f523d6a4896281cdc78e00f23b0b75ba146b0779
pnanipappala-dev <pnani.pappala@gmail.com>
2026-03-15 10:03:55 +0530

    Update overlay.js

--- www/overlay.js
+++ www/overlay.js
@@ -3,7 +3,6 @@ var exec = require('cordova/exec');
 var OverlayDetection = {
     isOverlayEnabled: function(success, error) {
         exec(function(result) {
-            // result is now a JSON object
             success(result);
         }, error, "OverlayDetectionPlugin", "isOverlayEnabled", []);
     }
