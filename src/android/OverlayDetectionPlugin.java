77163269c2ca01214dece3c316bb8624e7bfc5de
pnanipappala-dev <pnani.pappala@gmail.com>
2026-03-15 09:32:44 +0530

    Update OverlayDetectionPlugin.java

--- src/android/OverlayDetectionPlugin.java
+++ src/android/OverlayDetectionPlugin.java
@@ -1,28 +1,35 @@
 package com.overlay.security;
 
-import android.provider.Settings;
-import android.os.Build;
-import android.content.Context;
-
+import android.view.MotionEvent;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 
 public class OverlayDetectionPlugin extends CordovaPlugin {
 
+    private static boolean overlayDetected = false;
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
 
         if (action.equals("isOverlayEnabled")) {
-
-            Context context = cordova.getActivity().getApplicationContext();
-            boolean overlayDetected = false;
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                overlayDetected = Settings.canDrawOverlays(context);
-            }
-
             callbackContext.success(overlayDetected ? 1 : 0);
             return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onDispatchTouchEvent(MotionEvent event) {
+
+        if ((event.getFlags() & MotionEvent.FLAG_WINDOW_IS_OBSCURED) != 0 ||
+            (event.getFlags() & MotionEvent.FLAG_WINDOW_IS_PARTIALLY_OBSCURED) != 0) {
+
+            overlayDetected = true;
+
+        } else {
+
+            overlayDetected = false;
         }
 
         return false;
